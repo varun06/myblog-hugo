492c1ae13742063abda3688e3cd57c0b4bf3ffb0
Varun <varunkuamr.saini06@gmail.com>
2015-05-25 19:54:56 -0500

    new post on go in action book notes

---
+++ content/posts/using-sectionReader.md
@@ -0,0 +1,6 @@
++++
+date = "2015-04-26T07:40:00-06:00"
+draft = true
+title = "using SectionReader in Go"
+
++++
--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# delete the public directory if exists.
+rm -rf public
+
+# create a submodule for deployment
+git submodule add git@github.com:<varun06>/<varun06>.github.io.git public
+
 # Build the project.
 hugo -t casper # if using a theme, replace by `hugo -t <yourtheme>`
 
@@ -22,3 +28,9 @@ git push origin master
 
 # Come Back
 cd ..
+
+# delete the submodule as I am having issue with deployment.
+git submodule deinit public
+git rm public
+git rm --cached public
+rm -rf .git/modules/public
