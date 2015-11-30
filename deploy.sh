0d0e72cfa869563c466660672ecc5dc9ffd0f802
Varun <varunkumar.saini06@gmail.com>
2015-11-29 19:46:10 -0600

    new layout and theme

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,10 @@
 #!/bin/bash
-
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-# delete the public directory if exists.
-# rm -rf public
-
-# create a submodule for deployment
-# git submodule add git@github.com:varun06/varun06.github.io.git public
-
-# Build the project.
-hugo -t material-design # if using a theme, replace by `hugo -t <yourtheme>`
-
-# Go To Public folder
-cd public
-# Add changes to git.
-git add -A
-
-# Commit changes.
-msg="rebuilding site `date`"
+msg="update blog"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Come Back
-cd ..
-
-# delete the submodule as I am having issue with deployment.
-# git submodule deinit public
-# git rm public
-# git rm --cached public
-# rm -rf .git/modules/public
+git add -A && git commit -m "$msg" && git push origin master
+hugo
+cd public/
+git add -A && git commit -m "$msg" && git push origin master
+cd ../
